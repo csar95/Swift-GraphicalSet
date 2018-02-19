cf9c68bf9fbfa8d694d536859e74438d8ba13086
Cesar Gutierrez Carrero <cesarguti47@gmail.com>
2018-02-19 19:31:56 +0100

    Added drawing shade feature.

--- Game Set/BoardView.swift
+++ Game Set/BoardView.swift
@@ -26,36 +26,36 @@ class BoardView: UIView {
         let roundedRect = UIBezierPath(roundedRect: grid[index]!, cornerRadius: grid.cellSize.height * cornerRadiusCoefficient)
         UIColor.white.setFill()
         roundedRect.fill()
-        drawShapes(for: index, in: grid, repeat: Numbers.two, shape: Shapes.squiggle, with: Colors.purple)
+        drawShapes(for: index, in: grid, repeat: Numbers.three, shape: Shapes.squiggle, with: Colors.red, shade: Shading.solid)
     }
     
-    private func drawShapes(for index: Int, in grid: Grid, repeat times: Numbers, shape: Shapes, with color: Colors)
+    private func drawShapes(for index: Int, in grid: Grid, repeat times: Numbers, shape: Shapes, with color: Colors, shade: Shading)
     {
         let cornerOffSet = (grid.cellSize.height * cornerRadiusCoefficient) * cornerOffsetCoefficient
         let boxRect = grid[index]!.insetBy(dx: cornerOffSet, dy: cornerOffSet)
         
         switch times {
         case .one:
-            if (shape == .oval) { drawOval(within: boxRect, with: color) }
-            else if (shape == .diamond) { drawDiamond(within: boxRect, with: color) }
-            else { drawSquiggle(within: boxRect, with: color) }
+            if (shape == .oval) { drawOval(within: boxRect, with: color, shade: shade) }
+            else if (shape == .diamond) { drawDiamond(within: boxRect, with: color, shade: shade) }
+            else { drawSquiggle(within: boxRect, with: color, shade: shade) }
             
             break
         case .two:
             let (upperRect, lowerRect) = boxRect.divided(atDistance: boxRect.height/2, from: .minYEdge)
             
-            if (shape == .oval) { drawOval(within: upperRect, with: color); drawOval(within: lowerRect, with: color) }
-            else if (shape == .diamond) { drawDiamond(within: upperRect, with: color); drawDiamond(within: lowerRect, with: color) }
-            else { drawSquiggle(within: upperRect, with: color); drawSquiggle(within: lowerRect, with: color) }
+            if (shape == .oval) { drawOval(within: upperRect, with: color, shade: shade); drawOval(within: lowerRect, with: color, shade: shade) }
+            else if (shape == .diamond) { drawDiamond(within: upperRect, with: color, shade: shade); drawDiamond(within: lowerRect, with: color, shade: shade) }
+            else { drawSquiggle(within: upperRect, with: color, shade: shade); drawSquiggle(within: lowerRect, with: color, shade: shade) }
             
             break
         case .three:
             let (upperRect, auxRect) = boxRect.divided(atDistance: boxRect.height/3, from: .minYEdge)
             let (middleRect, lowerRect) = auxRect.divided(atDistance: auxRect.height/2, from: .minYEdge)
             
-            if (shape == .oval) { drawOval(within: upperRect, with: color); drawOval(within: middleRect, with: color); drawOval(within: lowerRect, with: color) }
-            else if (shape == .diamond) { drawDiamond(within: upperRect, with: color); drawDiamond(within: middleRect, with: color); drawDiamond(within: lowerRect, with: color) }
-            else { drawSquiggle(within: upperRect, with: color); drawSquiggle(within: middleRect, with: color); drawSquiggle(within: lowerRect, with: color) }
+            if (shape == .oval) { drawOval(within: upperRect, with: color, shade: shade); drawOval(within: middleRect, with: color, shade: shade); drawOval(within: lowerRect, with: color, shade: shade) }
+            else if (shape == .diamond) { drawDiamond(within: upperRect, with: color, shade: shade); drawDiamond(within: middleRect, with: color, shade: shade); drawDiamond(within: lowerRect, with: color, shade: shade) }
+            else { drawSquiggle(within: upperRect, with: color, shade: shade); drawSquiggle(within: middleRect, with: color, shade: shade); drawSquiggle(within: lowerRect, with: color, shade: shade) }
             
             break
         }
@@ -68,24 +68,17 @@ extension BoardView {
     private var cornerRadiusCoefficient: CGFloat { return 0.1 }
     private var cornerOffsetCoefficient: CGFloat { return 0.33 }
     
-    private func drawOval(within rect: CGRect, with color: Colors) {
+    private func drawOval(within rect: CGRect, with color: Colors, shade: Shading) {
         let cornerOffSetX = rect.width * cornerOffsetCoefficient
         let cornerOffSetY = rect.height * cornerOffsetCoefficient
         let ovalRect = rect.insetBy(dx: cornerOffSetX/2, dy: cornerOffSetY/1.5)
         
         let oval = UIBezierPath(ovalIn: ovalRect)
-        switch color {
-        case .green:
-            UIColor.green.setFill()
-        case .red:
-            UIColor.red.setFill()
-        case .purple:
-            UIColor.purple.setFill()
-        }
-        oval.fill()
+        
+        drawInside(of: oval, within: rect, with: color, shade: shade)
     }
     
-    private func drawDiamond(within rect: CGRect, with color: Colors) {
+    private func drawDiamond(within rect: CGRect, with color: Colors, shade: Shading) {
         let diamond = UIBezierPath()
         diamond.move(to: CGPoint( x: rect.midX, y: rect.midY + (rect.height/3) ))
         diamond.addLine(to: CGPoint( x: rect.midX + (rect.width/3), y: rect.midY ))
@@ -93,18 +86,10 @@ extension BoardView {
         diamond.addLine(to: CGPoint( x: rect.midX - (rect.width/3), y: rect.midY ))
         diamond.close()
         
-        switch color {
-        case .green:
-            UIColor.green.setFill()
-        case .red:
-            UIColor.red.setFill()
-        case .purple:
-            UIColor.purple.setFill()
-        }
-        diamond.fill()
+        drawInside(of: diamond, within: rect, with: color, shade: shade)
     }
     
-    private func drawSquiggle(within rect: CGRect, with color: Colors) {
+    private func drawSquiggle(within rect: CGRect, with color: Colors, shade: Shading) {
         let squiggle = UIBezierPath()
         squiggle.addArc(withCenter: CGPoint(x: rect.midX - (rect.width/2)/6, y: rect.midY), radius: (rect.width/2)/6, startAngle: 0, endAngle: 3*CGFloat.pi/2, clockwise: false)
         squiggle.addArc(withCenter: CGPoint(x: rect.midX, y: rect.midY - (rect.width/2)/6), radius: (rect.width/2)/6, startAngle: CGFloat.pi, endAngle: CGFloat.pi/2, clockwise: true)
@@ -112,14 +97,59 @@ extension BoardView {
         squiggle.addArc(withCenter: CGPoint(x: rect.midX, y: rect.midY + (rect.width/2)/6), radius: (rect.width/2)/6, startAngle: 0, endAngle: 3*CGFloat.pi/2, clockwise: true)
         squiggle.close()
         
-        switch color {
-        case .green:
-            UIColor.green.setFill()
-        case .red:
-            UIColor.red.setFill()
-        case .purple:
-            UIColor.purple.setFill()
+        drawInside(of: squiggle, within: rect, with: color, shade: shade)
+    }
+    
+    private func drawInside(of path: UIBezierPath, within rect: CGRect, with color: Colors, shade: Shading) {
+        if (shade == Shading.open) {
+            switch color {
+            case .green:
+                UIColor.green.setStroke()
+            case .red:
+                UIColor.red.setStroke()
+            case .purple:
+                UIColor.purple.setStroke()
+            }
+            path.stroke()
         }
-        squiggle.fill()
+        else if (shade == Shading.solid) {
+            switch color {
+            case .green:
+                UIColor.green.setFill()
+            case .red:
+                UIColor.red.setFill()
+            case .purple:
+                UIColor.purple.setFill()
+            }
+            path.fill()
+        }
+        else {
+            UIGraphicsGetCurrentContext()?.saveGState()
+            path.addClip()
+            switch color {
+            case .green:
+                UIColor.green.setStroke()
+            case .red:
+                UIColor.red.setStroke()
+            case .purple:
+                UIColor.purple.setStroke()
+            }
+            drawStripes(in: rect)
+            path.stroke()
+            UIGraphicsGetCurrentContext()?.restoreGState()
+        }
+    }
+    
+    private func drawStripes(in rect: CGRect) {
+        let numberOfStripes = 15
+        let path = UIBezierPath()
+        
+        let step = Int(rect.width)/numberOfStripes
+        
+        for stripe in 1...numberOfStripes {
+            path.move(to: CGPoint(x: rect.minX + CGFloat(step*stripe), y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.minX + CGFloat(step*stripe), y: rect.maxY))
+        }
+        path.stroke()
     }
 }
